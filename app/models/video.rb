a14b8c4ab4fcade7d107a67c96d380031fa4f723
Tim Jedrek <tim@timjedrek.com>
2022-09-18 21:08:49 -0500

    add scopes

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,8 @@
 class Video < ApplicationRecord
+  validates :title, presence: true
+  validates :code, presence: true
+
+  scope :music_videos, -> {where('category = ?', 'Music Videos')}
+  scope :battle_rap, -> {where('category = ?', 'Battle Rap')}
+  scope :artists_to_watch, -> {where('category = ?', 'Artists to Watch')}
 end
