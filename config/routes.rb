5c512ff108cbb162e4bdaaeccefd4eedbb311151
Tim Jedrek <tim@timjedrek.com>
2022-09-21 21:09:18 -0500

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'messages/confirmation', to: 'messages#confirmation'
-  
   resources :messages
   resources :videos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,9 +7,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  # VIDEO ROUTES
   get 'music-videos', to: 'videos#music_videos'
   get 'battle-rap', to: 'videos#battle_rap'
   get 'artists-to-watch', to: 'videos#artists_to_watch'
+  
+  # MESSAGE ROUTES
+  get 'contact', to: 'messages#new'
+  get 'contact/confirmation', to: 'messages#confirmation'
 
   root "pages#home"
 end
