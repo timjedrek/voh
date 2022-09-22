b07f40a5874eacfb4c04b2123f8eb2691adb13e4
Tim Jedrek <tim@timjedrek.com>
2022-09-21 20:29:18 -0500

    migrate db for messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_19_015334) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_011451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.datetime "created_at", null: false
