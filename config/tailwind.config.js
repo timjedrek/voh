d37bf56e23f525609113d1c4e8b67b0d0291306a
Tim Jedrek <tim@timjedrek.com>
2022-09-24 18:40:49 -0500

    add game corner content

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         splash: ['"Splash"', 'Helvetica', 'Arial', 'sans-serif'],
       },
+      backgroundImage : {
+        'gamecorner' : "url('jayrone-game-header.png')",
+      }
     },
   },
   plugins: [
