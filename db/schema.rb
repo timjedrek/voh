a7195f84930a3b4f98cdb2b94a1a48e9a3cf8964
Tim Jedrek <tim@timjedrek.com>
2022-09-27 09:55:31 -0500

    create feature model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_023341) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_145452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "features", force: :cascade do |t|
+    t.string "title"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "name"
