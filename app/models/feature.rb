9cd17a6a89c1c5a1ad1a7c7684af7dde96a40b94
Tim Jedrek <tim@timjedrek.com>
2022-09-27 12:28:05 -0500

    limit features and change video size

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -5,7 +5,8 @@ class Feature < ApplicationRecord
   private
 
   def only_one
-    if Feature.count >= 1
+    if Feature.count <= 1
+    else
       errors.add :base, 'There can only be one featured video' 
     end
   end
