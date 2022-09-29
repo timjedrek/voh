4574fba05058dd1d94d15dbb15e796dfe3f5ba1b
Tim Jedrek <tim@timjedrek.com>
2022-09-28 20:18:45 -0500

    validate tours

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,4 +1,6 @@
 class Tour < ApplicationRecord
+  validates :date, presence: true
+  validates :city, presence: true
 
   scope :upcoming, -> {where('date >= ?', Time.now)}
   scope :previous, -> {where('date < ?', Time.now)}
