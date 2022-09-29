92f6179c68f390dee117345adf1c7c8086bef5ba
Tim Jedrek <tim@timjedrek.com>
2022-09-28 20:16:15 -0500

    add message validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :content, presence: true
+  validates :email, presence: true
 end
