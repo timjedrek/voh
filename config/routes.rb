3443fc824d5ade3c4912e1eb22ebbe25a0a30a98
Tim Jedrek <tim@timjedrek.com>
2022-09-29 20:26:29 -0500

    add error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 
   devise_scope :admin do
     # Redirests signing out users back to sign-in
