90a5ed34914120ae4c1cffdfe927eb055ea56a8d
Tim Jedrek <tim@timjedrek.com>
2022-12-26 06:21:04 -0600

    change error page on development and remove migration file for new database set up due to postgres update on local machine

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,9 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = false
+  # true = show reports
+  # false = show custom error page (production)
+  config.consider_all_requests_local = true
 
   # Enable server timing
   config.server_timing = true
