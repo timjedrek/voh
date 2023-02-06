ed75fdfde1f21c9e07c1eb34e4bbc409a812e3d5
Tim Jedrek <tim@timjedrek.com>
2023-02-06 01:40:34 -0600

    change message

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -9,6 +9,7 @@ InvisibleCaptcha.setup do |config|
 
   # Leave these unset if you want to use I18n (see below)
   # config.sentence_for_humans     = 'If you are a human, ignore this field'
+  config.sentence_for_humans     = 'Ignore this field, no need to confirm email'
   # config.timestamp_error_message = 'Sorry, that was too quick! Please resubmit.'
   config.timestamp_error_message = 'Sorry, something went wrong! Please resubmit.'
 end
